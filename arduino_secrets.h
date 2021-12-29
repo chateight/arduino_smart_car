8f08d59c2cefb33e18ae465d1fcf3b7346d015c1
宇佐美隆一 <80155159+chateight@users.noreply.github.com>
2021-12-29 13:16:18 +0900

    Delete arduino_secrets.h

--- arduino_secrets.h
+++
@@ -1,5 +0,0 @@
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-char ssid[] = "yyy";        // your network SSID (name)
-char pass[] = "xxx";    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                 // your network key index number (needed only for WEP)
