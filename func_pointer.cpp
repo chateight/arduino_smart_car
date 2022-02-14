1bc4bbe93caf885c603feb9b57ca2ffb0b200625
ryuichi <rusami.usual@icloud.com>
2022-02-14 17:48:35 +0900

    # to add callback sample

--- func_pointer.cpp
+++ func_pointer.cpp
@@ -1,32 +1,55 @@
+/*
+ from http://pract.webcrow.jp/pages/contents/c/pointer/pointer-of-func
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-void start()
-{
+typedef void (*FUNC_POINTER)(int, int);
+
+void add(int a, int b){
+    printf("a + b = %d\n", a + b);
+}
+
+void sub(int a, int b){
+    printf("a - b = %d\n", a - b);
+}
+
+void calc(int a, int b, FUNC_POINTER p){        // using callback function
+    (*p)(a, b);
+}
+//
+//
+void start(){
     printf("Start\n");
 }
 
-void save()
-{
+void save(){
     printf("Save\n");
 }
 
-void end()
-{
+void end(){
     printf("End\n");
     exit(0);
 }
 
-void (*menu[3])() =
-{
+void (*menu[3])() ={    // define the function pointer array
     start, save, end
 };
 
-int main()
-{
+int main(){
+    // run the callback function example
+    FUNC_POINTER p;
+    p = add;
+    calc(100, 100, p);
+    p = sub;
+    calc(100, 100, p);
+    //
+    // run the funcion pointer example
+    // console input number 1,2,3(exit) defines the function pointer
+    //
     while(1){
         int i = getchar();
-        i -= '0' + 1;
+        i -= ('0' + 1);
         if( 0 <= i && i < 3 )
             menu[i]();
     }
